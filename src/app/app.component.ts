04b4dcd82483df4e985bb97597526139b0b9eca0
DomanTom07 <tamasdoman7@citromail.hu>
2022-11-29 20:25:21 +0100

    Bekérés, számolás kész

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,13 @@
+/*
+File: app.component.ts
+Author: Tamás Domán
+Copyright: 2022, Tamás Domán
+Group: Szoft II N
+Date: 2022-11-29
+Github: https://github.com/DomanTom07/
+Licenc: GNU GPL
+*/
+
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +17,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'dolgozat2';
+  aSide !: number;
+  bSide !: number;
+  area !: number;
+
+  calcArea():void {
+    this.area = 2 * this.aSide * this.bSide;
+    console.log("mukodik")
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,15 @@
+/*
+File: app.module.ts
+Author: Tamás Domán
+Copyright: 2022, Tamás Domán
+Group: Szoft II N
+Date: 2022-11-29
+Github: https://github.com/DomanTom07/
+Licenc: GNU GPL
+*/
+
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
@@ -8,7 +19,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
